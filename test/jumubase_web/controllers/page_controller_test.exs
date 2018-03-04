83a328eab160bf96eda79e54916c2db3f6005ca1
Martin Richter <me@martinrichter.net>
2018-03-04 01:19:53 +0100

    Fix test for home route

--- test/jumubase_web/controllers/page_controller_test.exs
+++ test/jumubase_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule JumubaseWeb.PageControllerTest do
   use JumubaseWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Jumu Nordost"
   end
 end

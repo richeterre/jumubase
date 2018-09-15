b290a993badcec33b5c8cf42bd1db041a054888a
Martin Richter <me@martinrichter.net>
2018-09-15 21:06:47 +0200

    Create some initial data with seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,6 +1,57 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# It is also run when you use the command `mix ecto.setup`
-#
+alias Ecto.Changeset
+alias Jumubase.Repo
+alias Jumubase.Accounts.User
+alias Jumubase.Foundation.Host
+
+Repo.transaction fn ->
+  # Clear existing data
+  Repo.delete_all(Host)
+  Repo.delete_all(User)
+
+  # Create demo hosts
+
+  _host1 = Repo.insert!(%Host{name: "DS Helsinki", city: "Helsinki", country_code: "FI", time_zone: "Europe/Helsinki"})
+  host2 = Repo.insert!(%Host{name: "DS Stockholm", city: "Stockholm", country_code: "SE", time_zone: "Europe/Stockholm"})
+  host3 = Repo.insert!(%Host{name: "DS Dublin", city: "Dublin", country_code: "IE", time_zone: "Europe/Dublin"})
+
+  # Create demo users
+
+  User.create_changeset(%User{}, %{
+    first_name: "Anna",
+    last_name: "Admin",
+    email: "admin@example.org",
+    password: "password",
+    role: "admin"
+  })
+  |> Repo.insert!
+
+  User.create_changeset(%User{}, %{
+    first_name: "Lukas",
+    last_name: "Landeswetter",
+    email: "lw-org@example.org",
+    password: "password",
+    role: "lw-organizer"
+  })
+  |> Repo.insert!
+
+  User.create_changeset(%User{}, %{
+    first_name: "Rieke",
+    last_name: "Regionalwetter",
+    email: "rw-org@example.org",
+    password: "password",
+    role: "rw-organizer"
+  })
+  |> Repo.insert!
+
+  User.create_changeset(%User{}, %{
+    first_name: "Ivo",
+    last_name: "Inspektor",
+    email: "inspektor@example.org",
+    password: "password",
+    role: "inspector"
+  })
+  |> Repo.insert!
+
+  # Confirm all users
+  Repo.update_all(User, set: [confirmed_at: DateTime.utc_now()])
+end

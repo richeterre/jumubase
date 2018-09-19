0df28d6d3e855c7badf1a092fe3ac8660236a76d
Martin Richter <me@martinrichter.net>
2018-09-19 22:10:37 +0200

    Edit recent Showtime migrations to enforce foreign key presence on DB level
    This ensures that performances, appearances and contest categories are always linked to their respective associations.

--- priv/repo/migrations/20180917213345_create_appearances.exs
+++ priv/repo/migrations/20180917213345_create_appearances.exs
@@ -3,8 +3,8 @@ defmodule Jumubase.Repo.Migrations.CreateAppearances do
 
   def change do
     create table(:appearances) do
-      add :performance_id, references(:performances, on_delete: :delete_all)
-      add :participant_id, references(:participants, on_delete: :delete_all)
+      add :performance_id, references(:performances, on_delete: :delete_all), null: false
+      add :participant_id, references(:participants, on_delete: :delete_all), null: false
       add :participant_role, :string
       add :instrument, :string
       add :points, :integer

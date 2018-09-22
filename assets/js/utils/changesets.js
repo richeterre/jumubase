0d8ee391ce9e0d46f721efa06ac0302bcbee9442
Denis Washington <denis@denisw.de>
2018-09-22 13:33:51 +0200

    Add docs for JS changeset utilitities

--- assets/js/utils/changesets.js
+++ assets/js/utils/changesets.js
@@ -1,5 +1,13 @@
 import { defaults, isArray, map, mapValues } from 'lodash'
 
+/**
+ * Merges an Ecto changeset's `changes` and `data` values into a
+ * single values object, preferring `changes` (changed values) over
+ * `data` (original values) if present. This is done recursively
+ * for nested changesets as well.
+ *
+ * @param {Object} changeset
+ */
 export function flattenChangesetValues(changeset) {
   const result = defaults({}, changeset.changes, changeset.data)
 

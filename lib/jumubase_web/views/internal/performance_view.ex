f34d05d15173a9e6d78bc7a9068f33e9052c5629
Martin Richter <me@martinrichter.net>
2018-09-22 11:20:11 +0200

    Show combined category and age group on performance detail page

--- lib/jumubase_web/views/internal/performance_view.ex
+++ lib/jumubase_web/views/internal/performance_view.ex
@@ -2,9 +2,13 @@ defmodule JumubaseWeb.Internal.PerformanceView do
   use JumubaseWeb, :view
   import JumubaseWeb.Internal.AppearanceView, only: [acc: 1, instrument_name: 1, non_acc: 1]
   import JumubaseWeb.Internal.ParticipantView, only: [birthdate: 1, full_name: 1]
-  alias Jumubase.Showtime.Performance
+  alias Jumubase.Showtime.{Appearance, Performance}
 
   def category_name(%Performance{} = performance) do
     performance.contest_category.category.name
+  end
+
+  def category_info(%Performance{} = performance) do
+    "#{category_name(performance)}, AG #{performance.age_group}"
   end
 end

045732dfb728b8ce1e644f15c34ea280306f06f4
Martin Richter <me@martinrichter.net>
2018-09-25 01:23:02 +0200

    Show age group badge for each appearance in performance detail page

--- lib/jumubase_web/views/internal/appearance_view.ex
+++ lib/jumubase_web/views/internal/appearance_view.ex
@@ -1,5 +1,6 @@
 defmodule JumubaseWeb.Internal.AppearanceView do
   use JumubaseWeb, :view
+  alias Jumubase.Showtime.Appearance
 
   @doc """
   Returns a display name for the instrument.
@@ -12,13 +13,20 @@ defmodule JumubaseWeb.Internal.AppearanceView do
   Returns only the accompanist appearances from the list.
   """
   def acc(appearances) do
-    Enum.filter(appearances, fn %{participant_role: role} ->
-      role == "accompanist"
-    end)
+    Enum.filter(appearances, &Appearance.is_accompanist/1)
   end
 
   @doc """
   Returns only the soloist and ensemblist appearances from the list.
   """
-  def non_acc(appearances), do: appearances -- acc(appearances)
+  def non_acc(appearances) do
+    Enum.filter(appearances, &!Appearance.is_accompanist(&1))
+  end
+
+  @doc """
+  Creates a badge for an age group.
+  """
+  def age_group_badge(%Appearance{age_group: ag}) do
+    content_tag :span, ag, class: "badge"
+  end
 end
--- lib/jumubase_web/views/internal/performance_view.ex
+++ lib/jumubase_web/views/internal/performance_view.ex
@@ -1,8 +1,10 @@
 defmodule JumubaseWeb.Internal.PerformanceView do
   use JumubaseWeb, :view
-  import JumubaseWeb.Internal.AppearanceView, only: [acc: 1, instrument_name: 1, non_acc: 1]
+  import JumubaseWeb.Internal.AppearanceView, only: [
+    acc: 1, age_group_badge: 1, instrument_name: 1, non_acc: 1
+  ]
   import JumubaseWeb.Internal.ParticipantView, only: [birthdate: 1, full_name: 1]
-  alias Jumubase.Showtime.{Appearance, Performance}
+  alias Jumubase.Showtime.Performance
 
   def category_name(%Performance{} = performance) do
     performance.contest_category.category.name

0e09a9678bba051114f0bd30ef9fcc5150977080
Martin Richter <me@martinrichter.net>
2018-10-20 21:31:02 +0200

    Fix Elixir warning about quoted "test" key in mix.exs

--- mix.exs
+++ mix.exs
@@ -58,9 +58,9 @@ defmodule Jumubase.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      {:"ecto.setup", ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"]},
+      {:"ecto.reset", ["ecto.drop", "ecto.setup"]},
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end

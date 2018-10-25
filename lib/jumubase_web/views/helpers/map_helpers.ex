8792912fcb04f6fd5ea51d7b227138d3cd4f2196
Martin Richter <me@martinrichter.net>
2018-10-25 16:14:13 +0200

    Add Mediterranean schools to host map

--- lib/jumubase_web/views/helpers/map_helpers.ex
+++ lib/jumubase_web/views/helpers/map_helpers.ex
@@ -7,10 +7,9 @@ defmodule JumubaseWeb.MapHelpers do
     base_url = "https://maps.googleapis.com/maps/api/staticmap?scale=2&size=640x400"
 
     markers =
-      host_coordinates()
-      |> Enum.reduce("", fn {lat, lon}, acc ->
-        acc <> "&markers=color:green%7C#{lat},#{lon}"
-      end)
+      get_markers(green_host_coordinates(), "green") <>
+      get_markers(yellow_host_coordinates(), "yellow") <>
+      get_markers(blue_host_coordinates(), "blue")
 
     styles =
       "&style=element:labels%7Cvisibility:off" <>
@@ -28,7 +27,14 @@ defmodule JumubaseWeb.MapHelpers do
 
   # Private helpers
 
-  defp host_coordinates do
+  defp get_markers(coordinates, color) do
+    coordinates
+    |> Enum.reduce("", fn {lat, lon}, acc ->
+      acc <> "&markers=color:#{color}%7C#{lat},#{lon}"
+    end)
+  end
+
+  defp green_host_coordinates do
     [
       {48.1491521,17.103554}, # DS Bratislava
       {50.8519895,4.4926787}, # DS Brüssel
@@ -46,6 +52,36 @@ defmodule JumubaseWeb.MapHelpers do
       {42.6691648,23.3492821}, # DS Sofia
       {59.3422421,18.0699085}, # DS Stockholm
       {52.1577924,21.0691116}, # DS Warschau
+    ]
+  end
+
+  defp blue_host_coordinates do
+    [
+      {41.3828055, 2.0888186}, # DS Barcelona
+      {43.259737, -2.909638}, # DS Bilbao
+      {28.0815404, -15.4696271}, # DS Gran Canaria
+      {38.7589417, -9.1634023}, # DS Lissabon
+      {40.5068422, -3.7054852}, # DS Madrid
+      {36.5293663, -4.7536925}, # DS Marbella
+      {41.1544284, -8.6375453}, # DS Porto
+      {43.2982822, -1.9905614}, # DS San Sebastian
+      {37.3864758, -5.9707329}, # DS Sevilla
+      {28.40612, -16.55074}, # DS Teneriffa
+      {39.482104, -0.363545}, # DS Valencia
+    ]
+  end
+
+  defp yellow_host_coordinates do
+    [
+      {31.1920129, 29.897566}, # DS Alexandria
+      {38.0367489, 23.7941474}, # DS Athen
+      {31.7960191, 35.2307578}, # DS Jerusalem
+      {41.0279912, 28.9757692}, # DS Istanbul
+      {30.0323808, 31.2108091}, # DS Kairo-West
+      {30.026485, 31.423194}, # DS Kairo-Ost
+      {45.4761567, 9.1820931}, # DS Mailand
+      {41.890161, 12.427013}, # DS Rom
+      {40.5757672, 22.9863796}, # DS Thessaloniki
     ]
   end
 

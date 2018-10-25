757838b11d891035dfaf944b0b056f52f0018688
Martin Richter <me@martinrichter.net>
2018-10-25 16:15:13 +0200

    Style website title using musical note

--- lib/jumubase_web/views/layout_view.ex
+++ lib/jumubase_web/views/layout_view.ex
@@ -2,7 +2,7 @@ defmodule JumubaseWeb.LayoutView do
   use JumubaseWeb, :view
   import JumubaseWeb.BreadcrumbHelpers
 
-  def title, do: "Jumu | weltweit"
+  def title, do: "Jumu ♫ weltweit"
 
   @doc """
   Tells whether the given breadcrumb is active.

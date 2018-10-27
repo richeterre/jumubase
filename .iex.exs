35a5bf1ba950680e01ee5be9ca2fc56f721f02c7
Martin Richter <me@martinrichter.net>
2018-10-27 13:11:47 +0200

    Add User alias to IEx config

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,4 @@
 alias Jumubase.Repo, warn: false
+alias Jumubase.Accounts.User, warn: false
 alias Jumubase.Foundation.{Category, Contest, ContestCategory, Host}, warn: false
 alias Jumubase.Showtime.{Appearance, Participant, Performance, Piece}, warn: false

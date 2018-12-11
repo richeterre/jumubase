5462ac82158549e17c7afd234484e9500534d417
Martin Richter <me@martinrichter.net>
2018-12-11 21:18:22 +0200

    Add buttons for inserting resizable breaks into schedule

--- assets/js/views/scheduler.js
+++ assets/js/views/scheduler.js
@@ -2,12 +2,21 @@ import $ from 'jquery'
 import 'jquery-ui/ui/widgets/sortable'
 import 'jquery-ui/ui/widgets/resizable'
 
-$(".sortable").sortable({
-  connectWith: ".sortable",
-}).disableSelection();
-
-$(".resizable").resizable({
-  grid: 10,
+const resizeConfig = {
+  grid: [0, 10],
   minHeight: 40,
   handles: "s",
-});
+}
+
+$(".schedule-column").sortable({
+  connectWith: ".schedule-column",
+})
+
+$(".spacer").resizable(resizeConfig)
+
+$(".add-spacer-button").click(function (e) {
+  e.preventDefault();
+  const sortableId = $(e.target).attr("data-target")
+  const spacer = $(`<div class="schedule-item spacer">Pause</div>`)
+  $(`#${sortableId}`).append(spacer.resizable(resizeConfig))
+})

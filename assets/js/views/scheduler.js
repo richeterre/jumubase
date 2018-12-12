7604d3410650b3c7abf736bf23cf70f29d6b2868
Martin Richter <me@martinrichter.net>
2018-12-12 18:22:28 +0200

    Let users filter unscheduled performance by contest category
    Note that we need to apply the filter whenever the unscheduled list receives new items, because the item might not match the filter and should thus "disappear" right after being dropped, until the filter is changed by the user to reveal it again.

--- assets/js/views/scheduler.js
+++ assets/js/views/scheduler.js
@@ -8,15 +8,39 @@ const resizeConfig = {
   handles: "s",
 }
 
+// Make schedule columns sortable
 $(".schedule-column").sortable({
   connectWith: ".schedule-column",
+  receive: function() {
+    applyContestCategoryFilter()
+  },
 })
 
+// Make spacers resizable
 $(".spacer").resizable(resizeConfig)
 
+// Let users add spacers to a schedule column
 $(".add-spacer-button").click(function (e) {
   e.preventDefault();
   const sortableId = $(e.target).attr("data-target")
   const spacer = $(`<div class="schedule-item spacer">Pause</div>`)
   $(`#${sortableId}`).append(spacer.resizable(resizeConfig))
 })
+
+// Let users filter unscheduled list by contest category
+$("#cc-select").change(function() {
+  applyContestCategoryFilter()
+})
+
+function applyContestCategoryFilter() {
+  const ccId = $("#cc-select").val()
+  const unscheduledItems = $("#schedule-column-unscheduled .schedule-item")
+
+  if (ccId) {
+    // Show only items that match contest category id
+    unscheduledItems.hide().filter(`[data-cc-id=${ccId}]`).show()
+  } else {
+    // Show all
+    unscheduledItems.show()
+  }
+}
--- lib/jumubase_web/views/internal/stage_view.ex
+++ lib/jumubase_web/views/internal/stage_view.ex
@@ -1,6 +1,7 @@
 defmodule JumubaseWeb.Internal.StageView do
   use JumubaseWeb, :view
   import JumubaseWeb.Internal.ContestView, only: [name_with_flag: 1]
+  import JumubaseWeb.Internal.PerformanceView, only: [cc_filter_options: 1]
 
   def render("scripts.schedule.html", _assigns) do
     ~E(<script src="/js/scheduler.js"></script>)

6f992a212660ade4c95565d8dbafc83406ac6d90
Martin Richter <me@martinrichter.net>
2018-12-14 01:06:21 +0200

    Let users schedule performance by dragging them between date columns
    Stage times are not yet calculated correctly, nor are schedule items sized according to their duration. Breaks can be inserted, but won't be restored in the DOM yet after page reloads.

--- assets/js/views/scheduler.js
+++ assets/js/views/scheduler.js
@@ -2,45 +2,89 @@ import $ from 'jquery'
 import 'jquery-ui/ui/widgets/sortable'
 import 'jquery-ui/ui/widgets/resizable'
 
-const resizeConfig = {
-  grid: [0, 10],
-  minHeight: 40,
-  handles: "s",
-}
+const scheduler = options => {
+  const resizeConfig = {
+    grid: [0, 10],
+    minHeight: 40,
+    handles: "s",
+  }
 
-// Make schedule columns sortable
-$(".schedule-column").sortable({
-  connectWith: ".schedule-column",
-  receive: function() {
+  // Make schedule columns sortable
+  $(".schedule-column").sortable({
+    connectWith: ".schedule-column",
+    update: function() {
+      applyContestCategoryFilter()
+      submitColumn(getColumnData($(this)))
+    },
+  })
+
+  // Make spacers resizable
+  $(".spacer").resizable(resizeConfig)
+
+  // Let users add spacers to a schedule column
+  $(".add-spacer-button").click(function(e) {
+    e.preventDefault();
+    const targetDate = $(e.target).attr("data-target-date")
+    const spacer = $(`<div class="schedule-item spacer">Pause</div>`)
+    $(`[data-date=${targetDate}]`).append(spacer.resizable(resizeConfig))
+  })
+
+  // Let users filter unscheduled list by contest category
+  $("#cc-select").change(function() {
     applyContestCategoryFilter()
-  },
-})
+  })
 
-// Make spacers resizable
-$(".spacer").resizable(resizeConfig)
+  function applyContestCategoryFilter() {
+    const ccId = $("#cc-select").val()
+    const unscheduledItems = $('.schedule-column[data-date=""] .schedule-item')
 
-// Let users add spacers to a schedule column
-$(".add-spacer-button").click(function (e) {
-  e.preventDefault();
-  const sortableId = $(e.target).attr("data-target")
-  const spacer = $(`<div class="schedule-item spacer">Pause</div>`)
-  $(`#${sortableId}`).append(spacer.resizable(resizeConfig))
-})
+    if (ccId) {
+      // Show only items that match contest category id
+      unscheduledItems.hide().filter(`[data-cc-id=${ccId}]`).show()
+    } else {
+      // Show all
+      unscheduledItems.show()
+    }
+  }
 
-// Let users filter unscheduled list by contest category
-$("#cc-select").change(function() {
-  applyContestCategoryFilter()
-})
+  function submitColumn(data) {
+    const { contestId, csrfToken } = options
 
-function applyContestCategoryFilter() {
-  const ccId = $("#cc-select").val()
-  const unscheduledItems = $("#schedule-column-unscheduled .schedule-item")
+    $.ajax(`/internal/contests/${contestId}/performances/reschedule`, {
+      method: "PATCH",
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("X-CSRF-Token", csrfToken)
+      },
+      data: {
+        performances: data,
+      },
+    }).fail(function() {
+      alert("Something went wrong") // TODO
+    })
+  }
 
-  if (ccId) {
-    // Show only items that match contest category id
-    unscheduledItems.hide().filter(`[data-cc-id=${ccId}]`).show()
-  } else {
-    // Show all
-    unscheduledItems.show()
+  function getColumnData(column) {
+    const date = $(column).attr("data-date")
+    return $.makeArray(
+      $(column).children(".schedule-item").map(function(index, item) {
+        const id = $(item).attr("data-id")
+        const time = timeFromIndex(index)
+        return id && {
+          id,
+          stageId: date ? options.stageId : null,
+          stageTime: date ? `${date}T${time}` : null,
+        }
+      })
+    ).reduce((acc, item) => {
+      acc[item.id] = item
+      return acc
+    }, {})
+  }
+
+  function timeFromIndex(index) {
+    return `0${index}:00:00Z`
   }
 }
+
+// Make scheduler() available to global <script> tags
+window.scheduler = scheduler
--- lib/jumubase_web/views/internal/stage_view.ex
+++ lib/jumubase_web/views/internal/stage_view.ex
@@ -3,7 +3,16 @@ defmodule JumubaseWeb.Internal.StageView do
   import JumubaseWeb.Internal.ContestView, only: [name_with_flag: 1]
   import JumubaseWeb.Internal.PerformanceView, only: [cc_filter_options: 1]
 
-  def render("scripts.schedule.html", _assigns) do
-    ~E(<script src="/js/scheduler.js"></script>)
+  def render("scripts.schedule.html", assigns) do
+    options = render_html_safe_json %{
+      csrfToken: Plug.CSRFProtection.get_csrf_token(),
+      contestId: assigns.contest.id,
+      stageId: assigns.stage.id,
+    }
+
+    ~E{
+      <script src="/js/scheduler.js"></script>
+      <script>scheduler(<%= raw(options) %>)</script>
+    }
   end
 end

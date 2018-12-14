4a748227d166d2ab0f7bcabb20f89092c1b431c2
Martin Richter <me@martinrichter.net>
2018-12-14 18:42:33 +0200

    Improve category and appearance role mappings in XML

--- lib/jumubase_web/views/encoders/xml_encoder.ex
+++ lib/jumubase_web/views/encoders/xml_encoder.ex
@@ -20,8 +20,9 @@ defmodule JumubaseWeb.XMLEncoder do
     {:teilnehmer, %{id: a1.participant.id}, [
       to_xml(a1),
       {:wertung, nil, [
-        {:type, nil, map_type(cc.category.type)},
+        {:type, nil, map_role(a1.role)},
         {:instrument_stimmlage, nil, Instruments.name(a1.instrument)},
+        {:kategorie, nil, map_category(cc.category)},
       ]},
       {:spielpartner, nil, Enum.map(rest, &{:partner, %{id: &1.participant.id}, to_xml(&1)})}
     ]}
@@ -37,8 +38,11 @@ defmodule JumubaseWeb.XMLEncoder do
     ]
   end
 
-  defp map_type("solo"), do: "solo"
-  defp map_type("ensemble"), do: "gruppe"
+  defp map_role("soloist"), do: "solo"
+  defp map_role("ensemblist"), do: "gruppe"
+  defp map_role("accompanist"), do: "begleiter"
+  defp map_category(%{type: "solo"}), do: "A21"
+  defp map_category(%{type: "ensemble"}), do: "B60"
 
   defp format_date(date), do: Timex.format!(date, "{D}.{M}.{YYYY}")
 end

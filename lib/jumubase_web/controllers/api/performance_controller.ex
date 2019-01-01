d4f19f00eaa2f7728ebd05f99ea00ab003af885f
Martin Richter <me@martinrichter.net>
2019-01-01 14:58:55 +0100

    Allow filtering performances by contest category in REST API

--- lib/jumubase_web/controllers/api/performance_controller.ex
+++ lib/jumubase_web/controllers/api/performance_controller.ex
@@ -4,11 +4,14 @@ defmodule JumubaseWeb.Api.PerformanceController do
   alias Jumubase.Showtime
   alias Jumubase.Showtime.PerformanceFilter
 
-  def index(conn, %{"contest_id" => contest_id, "venue_id" => stage_id, "date" => date}) do
+  def index(conn, %{"contest_id" => contest_id, "venue_id" => stage_id, "date" => date} = params) do
     contest = Foundation.get_contest!(contest_id)
+    cc_id = params["contest_category_id"] # can be nil
+
     filter = %PerformanceFilter{
       stage_id: stage_id,
-      stage_date: Date.from_iso8601!(date)
+      stage_date: Date.from_iso8601!(date),
+      contest_category_id: cc_id,
     }
 
     performances = Showtime.list_performances(contest, filter) |> Showtime.load_pieces

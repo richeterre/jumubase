7d992debf0203817d5ad15946c1175826bdf814c
Martin Richter <me@martinrichter.net>
2019-01-01 15:20:50 +0100

    Fix date parsing in REST API to support legacy clients
    Unfortunately, some clients seem to send the `date` param for performance filtering in broken ISO format, because the day and month don't have leading zeroes. We use Timex to parse them nevertheless.

--- lib/jumubase_web/controllers/api/performance_controller.ex
+++ lib/jumubase_web/controllers/api/performance_controller.ex
@@ -6,11 +6,15 @@ defmodule JumubaseWeb.Api.PerformanceController do
 
   def index(conn, %{"contest_id" => contest_id, "venue_id" => stage_id, "date" => date} = params) do
     contest = Foundation.get_contest!(contest_id)
+
+    # Parse date, which some clients unfortunately send without leading zeroes (e.g. 2018-1-31)
+    date = Timex.parse!(date, "{YYYY}-{M}-{D}") |> NaiveDateTime.to_date
+
     cc_id = params["contest_category_id"] # can be nil
 
     filter = %PerformanceFilter{
       stage_id: stage_id,
-      stage_date: Date.from_iso8601!(date),
+      stage_date: date,
       contest_category_id: cc_id,
     }
 

a12087cdeffde1b2a1f256ab338d78ca87574f20
Martin Richter <me@martinrichter.net>
2019-01-02 19:18:31 +0100

    Clean up /performances endpoint logic in REST API
    This changes the controller to support two sets of params when asking for performances: Either by venue and date, or by contest category and results public flag.

--- lib/jumubase_web/controllers/api/performance_controller.ex
+++ lib/jumubase_web/controllers/api/performance_controller.ex
@@ -1,27 +1,42 @@
 defmodule JumubaseWeb.Api.PerformanceController do
   use JumubaseWeb, :controller
   alias Jumubase.Foundation
+  alias Jumubase.Foundation.Contest
   alias Jumubase.Showtime
   alias Jumubase.Showtime.PerformanceFilter
 
-  def index(conn, %{"contest_id" => contest_id, "venue_id" => stage_id, "date" => date} = params) do
+  def index(conn, %{"contest_id" => contest_id, "venue_id" => stage_id, "date" => date}) do
     contest = Foundation.get_contest!(contest_id)
 
     # Parse date, which some clients unfortunately send without leading zeroes (e.g. 2018-1-31)
-    date = Timex.parse!(date, "{YYYY}-{M}-{D}") |> NaiveDateTime.to_date
+    case Timex.parse(date, "{YYYY}-{M}-{D}") do
+      {:ok, datetime} ->
+        stage_date = NaiveDateTime.to_date(datetime)
+        filter = PerformanceFilter.from_params(%{stage_id: stage_id, stage_date: stage_date})
+        conn |> render_performances(contest, filter)
+      {:error, _} ->
+        handle_bad_request(conn)
+    end
+  end
+  def index(conn, %{"contest_id" => contest_id, "contest_category_id" => cc_id, "results_public" => results_public}) do
+    contest = Foundation.get_contest!(contest_id)
 
-    cc_id = params["contest_category_id"] # can be nil
+    filter = PerformanceFilter.from_params(%{contest_category_id: cc_id, results_public: results_public})
+    conn |> render_performances(contest, filter)
+  end
+  def index(conn, _params), do: handle_bad_request(conn)
 
-    filter = %PerformanceFilter{
-      stage_id: stage_id,
-      stage_date: date,
-      contest_category_id: cc_id,
-    }
+  # Private helpers
 
-    performances = Showtime.list_performances(contest, filter) |> Showtime.load_pieces
+  defp render_performances(conn, %Contest{} = c, %PerformanceFilter{} = filter) do
+    performances = Showtime.list_performances(c, filter) |> Showtime.load_pieces
 
     conn
     |> assign(:performances, performances)
     |> render("index.json")
+  end
+
+  defp handle_bad_request(conn) do
+    conn |> send_resp(400, "")
   end
 end

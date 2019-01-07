5c391d65d4f158b61b17e006133e00fabddf16ce
Martin Richter <me@martinrichter.net>
2019-01-07 17:40:57 +0100

    Remove unused ContestCategory.changeset/2
    So far, contest categories are only created using the Seeder module, which uses Jumubase.Factory directly.

--- lib/jumubase/foundation/contest_category.ex
+++ lib/jumubase/foundation/contest_category.ex
@@ -1,7 +1,5 @@
 defmodule Jumubase.Foundation.ContestCategory do
   use Jumubase.Schema
-  import Ecto.Changeset
-  alias Jumubase.Foundation.AgeGroups
   alias Jumubase.Foundation.{Category, Contest}
 
   schema "contest_categories" do
@@ -14,20 +12,5 @@ defmodule Jumubase.Foundation.ContestCategory do
     belongs_to :category, Category
 
     timestamps()
-  end
-
-  @required_attrs [:contest_id, :category_id, :min_age_group, :max_age_group]
-
-  @optional_attrs [:min_advancing_age_group, :max_advancing_age_group]
-
-  @doc false
-  def changeset(struct, attrs) do
-    struct
-    |> cast(attrs, @required_attrs ++ @optional_attrs)
-    |> validate_required(@required_attrs)
-    |> validate_inclusion(:min_age_group, AgeGroups.all)
-    |> validate_inclusion(:max_age_group, AgeGroups.all)
-    |> validate_inclusion(:min_advancing_age_group, AgeGroups.all)
-    |> validate_inclusion(:max_advancing_age_group, AgeGroups.all)
   end
 end

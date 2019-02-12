c492391d0811813b247cc8d0c41bb8c06160c98f
Martin Richter <me@martinrichter.net>
2019-02-12 11:55:48 +0100

    Handle empty state for duplicate participant list

--- lib/jumubase_web/views/internal/maintenance_view.ex
+++ lib/jumubase_web/views/internal/maintenance_view.ex
@@ -1,4 +1,11 @@
 defmodule JumubaseWeb.Internal.MaintenanceView do
   use JumubaseWeb, :view
   import JumubaseWeb.Internal.ParticipantView, only: [full_name: 1]
+
+  @doc """
+  Lists the participants' names using a simple separator.
+  """
+  def list_participants(participants) do
+    participants |> Enum.map(&full_name(&1)) |> Enum.join(" / ")
+  end
 end

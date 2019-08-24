0ebd2952b3413370a3e38c6a61a30f3f00caa10b
Martin Richter <me@martinrichter.net>
2019-08-25 00:36:49 +0200

    Configure application host in prod environment via env var

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :jumubase, JumubaseWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "www.jumu-nordost.eu", port: 443],
+  url: [scheme: "https", host: System.get_env("HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")

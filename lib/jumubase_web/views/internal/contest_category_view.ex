d926585abd5809461e49cc7a9730660f6553652f
Martin Richter <me@martinrichter.net>
2020-01-13 22:04:48 +0100

    Show groups_accompanists flag in contest category list

--- lib/jumubase_web/views/internal/contest_category_view.ex
+++ lib/jumubase_web/views/internal/contest_category_view.ex
@@ -2,4 +2,15 @@ defmodule JumubaseWeb.Internal.ContestCategoryView do
   use JumubaseWeb, :view
   import JumubaseWeb.Internal.AppearanceView, only: [badge: 1]
   import JumubaseWeb.Internal.ContestView, only: [name_with_flag: 1]
+  alias Jumubase.Foundation.ContestCategory
+
+  @doc """
+  Returns an info text explaining how accompanists are rated in the contest category.
+  """
+  def accompanist_rating_info(%ContestCategory{} = cc) do
+    case cc.groups_accompanists do
+      true -> gettext("as group")
+      false -> gettext("separately")
+    end
+  end
 end

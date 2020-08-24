9a1cb94a97982a522695fecc170ff2a32d3f7bb4
Martin Richter <me@martinrichter.net>
2020-08-24 15:03:59 +0200

    Use <small> element in form error tags

--- lib/jumubase_web/views/helpers/error_helpers.ex
+++ lib/jumubase_web/views/helpers/error_helpers.ex
@@ -11,7 +11,7 @@ defmodule JumubaseWeb.ErrorHelpers do
   """
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error), class: "help-block")
+      content_tag(:small, translate_error(error), class: "help-block")
     end)
   end
 

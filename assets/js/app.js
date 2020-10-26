2c38fd505c81b50c3466da8f91ca83aec729c135
Martin Richter <me@martinrichter.net>
2020-10-26 20:55:01 +0100

    Add polyfills to get LiveView support on IE11
    This follows the instructions at https://github.com/phoenixframework/phoenix_live_view#browser-support and should be reverted as soon as we can drop IE11 support.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,26 @@
 import "phoenix_html"
 import "bootstrap-sass"
+
+// Import polyfills needed for IE11 support
+// (see https://github.com/phoenixframework/phoenix_live_view#browser-support)
+import "mdn-polyfills/Object.assign"
+import "mdn-polyfills/CustomEvent"
+import "mdn-polyfills/String.prototype.startsWith"
+import "mdn-polyfills/Array.from"
+import "mdn-polyfills/Array.prototype.find"
+import "mdn-polyfills/Array.prototype.some"
+import "mdn-polyfills/NodeList.prototype.forEach"
+import "mdn-polyfills/Element.prototype.closest"
+import "mdn-polyfills/Element.prototype.matches"
+import "mdn-polyfills/Node.prototype.remove"
+import "child-replace-with-polyfill"
+import "url-search-params-polyfill"
+import "formdata-polyfill"
+import "classlist-polyfill"
+import "@webcomponents/template"
+import "shim-keyboard-event-key"
+import "core-js/features/set"
+
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 

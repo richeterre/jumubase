2b8e295daed8317a39efb64dc726366b27d18e7e
Martin Richter <me@martinrichter.net>
2021-01-16 20:06:39 +0200

    Improve display of ContestCategory flags in list
    Instead of always showing a text, we only display notes for flags that are switched on, to reduce visual clutter.

--- lib/jumubase_web/views/internal/contest_category_view.ex
+++ lib/jumubase_web/views/internal/contest_category_view.ex
@@ -5,12 +5,25 @@ defmodule JumubaseWeb.Internal.ContestCategoryView do
   alias Jumubase.Foundation.ContestCategory
 
   @doc """
-  Returns an info text explaining how accompanists are rated in the contest category.
+  Returns a linebreak-separated list of the contest category's flags, if any.
   """
-  def accompanist_rating_info(%ContestCategory{} = cc) do
-    case cc.groups_accompanists do
-      true -> gettext("as group")
-      false -> gettext("separately")
+  def notes(%ContestCategory{} = cc) do
+    [wespe_nomination_text(cc), accompanist_rating_text(cc)]
+    |> Enum.reject(&is_nil/1)
+    |> Enum.intersperse(Phoenix.HTML.Tag.tag(:br))
+  end
+
+  # Private helpers
+
+  defp wespe_nomination_text(%ContestCategory{} = cc) do
+    if cc.allows_wespe_nominations do
+      gettext("Allows WESPE nominations")
+    end
+  end
+
+  defp accompanist_rating_text(%ContestCategory{} = cc) do
+    if cc.groups_accompanists do
+      gettext("Accompanists rated as group")
     end
   end
 end

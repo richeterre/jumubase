86bd99106075cb2b92269e70e089d44a8eca0b75
Martin Richter <me@martinrichter.net>
2021-10-29 15:49:48 +0200

    Upgrade to Phoenix 1.6
    This follows (most of) the instructions at https://gist.github.com/chrismccord/2ab350f154235ad4a4d0f4de6decba7b

--- lib/jumubase_web.ex
+++ lib/jumubase_web.ex
@@ -36,7 +36,7 @@ defmodule JumubaseWeb do
         namespace: JumubaseWeb
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
+      import Phoenix.Controller, only: [get_flash: 2, view_module: 1, view_template: 1]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML

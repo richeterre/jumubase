efbd2eac6eeea936f20836a3be1d10bd87cc6a4e
Martin Richter <me@martinrichter.net>
2022-09-01 15:08:04 +0200

    Fix some deprecation warnings

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Jumubase.Mixfile do
       version: "1.0.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()

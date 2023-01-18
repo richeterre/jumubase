f2511f56f13855a3749ee8282348148d1f22e631
Martin Richter <me@martinrichter.net>
2023-01-18 17:35:14 +0200

    Remove obsolete admin email helper

--- lib/jumubase_web/views/internal/page_view.ex
+++ lib/jumubase_web/views/internal/page_view.ex
@@ -3,13 +3,7 @@ defmodule JumubaseWeb.Internal.PageView do
   import JumubaseWeb.Internal.ContestView, only: [name_with_flag: 1]
   import JumubaseWeb.PageView, only: [document_link: 2]
   alias JumubaseWeb.Endpoint
-  alias JumubaseWeb.Email
   alias JumubaseWeb.Internal.ContestLive
-
-  def admin_email do
-    config = Application.get_env(:jumubase, Email)
-    config[:admin_email]
-  end
 
   def jury_material_link(title, file_name) do
     document_link(
